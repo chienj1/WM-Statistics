1da384a35f6865d26003e9c12e7a1944d474a7a9
chienj1 <b02202037@ntu.edu.tw>
2017-06-03 21:34:03 +0800

    Update WFM.py

--- WFM.py
+++ WFM.py
@@ -1,33 +1,24 @@
-#{'code': 200,
-# 'response': {'buy': [{'count': 1,
-#    'ingame_name': 'Neverlight',
-#    'online_ingame': False,
-#    'online_status': False,
-#    'price': 20},
-#   {'count': 1,
-#    'ingame_name': 'Bress94',
-#    'online_ingame': False,
-#    ..........................................
-
 import requests, json
+import datetime
 
-item=['Blueprint/Dakra%20Prime%20Blade','Set/Ankyros%20Prime%20Set','Blueprint/Ankyros%20Prime%20Blade']
+BP=['Dakra Prime Blade','Ankyros Prime Blade']
+Set=['Dakra Prime Set']
+Mod=[]
+Cate={'Blueprint':BP_item,'Mod':Mod_item,'Set':Set_item}
 raw_url="https://warframe.market/api/get_orders/"
-for i in iter(item) :
-    print(i)
-    count = 0
-    res = requests.get(raw_url+i)
-    data = json.loads(res.text)
-    k=[]
-    for j in range(len(data['response']['sell'])):
-        if data['response']['sell'][j]['online_ingame'] == True:          
-            k.append(data['response']['sell'][j]['price'])
-            count+=1
-    k.sort()
-    print(k)
-    print('# of sellers : '+str(count))
-    
-    
-
-# add clock
-# let print to be in a list
+date = str(datetime.datetime.now())[0:13]                                   #
+for cate in iter(Cate):  
+    for item in iter(Cate[cate]):
+        count = 0
+        price_list=[]
+        file = open(item+'.txt', 'a+')
+        res = requests.get(raw_url+cate+'/'+item)
+        data = json.loads(res.text)
+        for i in range(len(data['response']['sell'])):
+            if data['response']['sell'][i]['online_ingame'] == True:        #alter : sell, buy
+                price_list.append(data['response']['sell'][i]['price'])      #alter : price, count, ingame_name, online_ingame, online_status
+                count+=1
+        price_list.sort()
+        print(date + '\t' + str(price_list[0])+ '\t' + str(count) +'\n')        
+        # file format: date, lowest price, number of sellers 
+        file.close() 
