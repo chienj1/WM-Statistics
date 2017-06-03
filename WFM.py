4174b09cf637bcebe9f4bcd8c5895860d64b28ba
chienj1 <b02202037@ntu.edu.tw>
2017-06-03 21:46:20 +0800

    Update WFM.py

--- WFM.py
+++ WFM.py
@@ -1,24 +1,29 @@
 import requests, json
 import datetime
 
-BP=['Dakra Prime Blade','Ankyros Prime Blade']
-Set=['Dakra Prime Set']
-Mod=[]
-Cate={'Blueprint':BP_item,'Mod':Mod_item,'Set':Set_item}
+# blueprint items
+bp=['Dakra Prime Blade','Vauban Prime System','Tigris Prime Blueprint','Nekros Prime Blueprint','Nekros Prime System','Valkyr Prime Chassis','Valkyr Prime System','Banshee Prime System','Reaper Prime Blade','Soma Prime Stock','Dual Kamas Prime Blade','Galatine Prime Blueprint']
+# Set items
+st=['Dakra Prime Set','Mag Prime Set','Nova Prime Set','Nyx Prime Set','Scindo Prime Set','Reaper Prime Set','Soma Prime Set']
+# Mod items
+md=['Vengeful Revenant','Sovereign Outcast','Empowered Blades','Growing Power','Enduring Affliction']
+Cate={'Blueprint':bp,'Mod':md,'Set':st}
+# warframe market
 raw_url="https://warframe.market/api/get_orders/"
-date = str(datetime.datetime.now())[0:13]                                   #
+# current time, up to Hour
+date = str(datetime.datetime.now())[0:13]
+
 for cate in iter(Cate):  
     for item in iter(Cate[cate]):
         count = 0
         price_list=[]
-        file = open(item+'.txt', 'a+')
         res = requests.get(raw_url+cate+'/'+item)
         data = json.loads(res.text)
+        file = open(item+'.txt', 'a+')
         for i in range(len(data['response']['sell'])):
             if data['response']['sell'][i]['online_ingame'] == True:        #alter : sell, buy
-                price_list.append(data['response']['sell'][i]['price'])      #alter : price, count, ingame_name, online_ingame, online_status
+                price_list.append(data['response']['sell'][i]['price'])     #alter : price, count, ingame_name, online_ingame, online_status
                 count+=1
         price_list.sort()
-        print(date + '\t' + str(price_list[0])+ '\t' + str(count) +'\n')        
-        # file format: date, lowest price, number of sellers 
-        file.close() 
+        print(date + '\t' + str(price_list[0])+ '\t' + str(count) +'\n')    # file format: date, lowest price, number of sellers     
+        file.close()
